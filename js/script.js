0a76bd9d282dd51c2802449af41a63eba168386f
Andestrial <45152356+Andestrial@users.noreply.github.com>
2018-11-26 19:34:23 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+menu.onclick = 	function myFanction(){
+	var x = document.getElementById('myTopnav');
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	}
+	else
+	{
+		x.className === "topnav";
+	}
+}
+  
+ $(document).ready(function(){
+    $("#myTopnav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
